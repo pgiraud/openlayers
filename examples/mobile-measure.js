5b0f6c96c9e990e14684272aa0041a179a5b5443
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-10-01 15:43:10 +0200

    Mobile workflow enhanced measure tool

---
+++ examples/mobile-measure.js
@@ -0,0 +1,19 @@
+var map;
+
+function init(){
+    // allow testing of specific renderers via "?renderer=Canvas", etc
+
+    map = new OpenLayers.Map('map', {
+    });
+    layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+    map.addLayer(layer);
+    map.setCenter(
+        new OpenLayers.LonLat(-71.147, 42.472).transform(
+            new OpenLayers.Projection("EPSG:4326"),
+            map.getProjectionObject()
+        ), 12
+    );    
+
+    var control = new MobileMeasure();
+    map.addControl(control);
+}
